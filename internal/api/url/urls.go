c2232bbbb8d5d304f656c492e8363c03751d123e
Benjamin Metz <ben@austerelabs.com>
2025-05-23 06:29:32 -0700

    adding url paths for anthropic, messages, gemini and grok

--- internal/api/url/urls.go
+++ internal/api/url/urls.go
@@ -1,6 +1,10 @@
 package url
 
 const (
-	Home  = "/v1/"
-	Users = "/v1/users/{name}"
+	Home              = "/v1/"
+	Users             = "/v1/users/{name}"
+	Messages          = "/v1/messages"
+	AnthropicMessages = "/v1/anthropic"
+	GeminiMessages    = "/v1/gemini-25flash"
+	GrokMessages      = "/v1/grok3"
 )
